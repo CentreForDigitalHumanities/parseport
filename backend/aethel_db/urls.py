e301c22f6cf643055e778ba6435468c61fc6dcd3
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-08-22 10:44:20 +0200

    Use lowercase filenames

--- backend/aethel_db/urls.py
+++ backend/aethel_db/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from aethel_db.views.AethelDetailView import AethelDetailView
-from aethel_db.views.AethelListView import AethelListView
-from aethel_db.views.AethelSampleDataView import AethelSampleDataView
+from aethel_db.views.detail import AethelDetailView
+from aethel_db.views.list import AethelListView
+from aethel_db.views.sample_data import AethelSampleDataView
 
 urlpatterns = [
     path("", AethelListView.as_view(), name="aethel-list"),
