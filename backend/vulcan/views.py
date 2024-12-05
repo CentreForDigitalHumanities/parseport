cc0ae4b022b9c63ed36d1ec8208e713b8b11ef54
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-12-05 16:52:50 +0100

    Remove unused route params

--- backend/vulcan/views.py
+++ backend/vulcan/views.py
@@ -4,9 +4,4 @@ from django.views import View
 # Create your views here.
 class VulcanView(View):
     def get(self, request, *args, **kwargs):
-        route_param = kwargs.get('id' , None)
-
-        print("Route param:", route_param)
-
-        context = {'hello': 'Hello, Vulcan!'}
-        return render(request, 'vulcan/index.html', context)
+        return render(request, 'vulcan/index.html')
