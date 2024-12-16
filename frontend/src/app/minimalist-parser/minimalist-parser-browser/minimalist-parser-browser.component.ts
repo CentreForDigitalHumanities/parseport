c02723d208ac0a611cd8f06967332f9543ee7c9f
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2024-12-16 14:34:46 +0100

    Styling updates

--- frontend/src/app/minimalist-parser/minimalist-parser-browser/minimalist-parser-browser.component.ts
+++ frontend/src/app/minimalist-parser/minimalist-parser-browser/minimalist-parser-browser.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import { map } from "rxjs";
 import { StatusService } from "src/app/shared/services/status.service";
 import { environment } from "src/environments/environment";
@@ -13,10 +14,14 @@ export class MinimalistParserBrowserComponent {
         .getStatus$()
         .pipe(map((status) => status.vulcan));
 
+    public icons = {
+        openInNewTab: faExternalLinkAlt,
+    };
+
     constructor(private statusService: StatusService) {}
 
     public navigateToVulcan(): void {
         const origin = window.location.origin;
-        window.location.href = `${origin}${environment.vulcanUrl}`;
+        window.open(`${origin}${environment.vulcanUrl}`, "_blank");
     }
 }
