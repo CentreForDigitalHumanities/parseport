8058eefc53300a61a9c17a4813dc72feea467f74
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-10-20 17:36:44 +0200

    Fix failing frontend tests

--- frontend/src/app/minimalist-parser/minimalist-parser-about/minimalist-parser-about.component.spec.ts
+++ frontend/src/app/minimalist-parser/minimalist-parser-about/minimalist-parser-about.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { RouterModule } from "@angular/router";
 import { MinimalistParserAboutComponent } from "./minimalist-parser-about.component";
+import { routes } from "src/app/routes";
 
 describe("MinimalistParserAboutComponent", () => {
     let component: MinimalistParserAboutComponent;
@@ -8,6 +9,7 @@ describe("MinimalistParserAboutComponent", () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [RouterModule.forRoot(routes)],
             declarations: [MinimalistParserAboutComponent],
         }).compileComponents();
 
--- frontend/src/app/minimalist-parser/minimalist-parser-browser/minimalist-parser-browser.component.spec.ts
+++ frontend/src/app/minimalist-parser/minimalist-parser-browser/minimalist-parser-browser.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { MinimalistParserBrowserComponent } from "./minimalist-parser-browser.component";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { SharedModule } from "src/app/shared/shared.module";
+import { RouterModule } from "@angular/router";
+import { routes } from "src/app/routes";
 
 describe("MinimalistParserBrowserComponent", () => {
     let component: MinimalistParserBrowserComponent;
@@ -11,7 +13,7 @@ describe("MinimalistParserBrowserComponent", () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [MinimalistParserBrowserComponent],
-            imports: [HttpClientTestingModule, SharedModule],
+            imports: [HttpClientTestingModule, SharedModule, RouterModule.forRoot(routes)],
         }).compileComponents();
 
         fixture = TestBed.createComponent(MinimalistParserBrowserComponent);
