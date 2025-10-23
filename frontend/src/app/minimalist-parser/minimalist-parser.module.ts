824dce74cd25db969f19b662b974ac00341bed06
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-10-23 15:59:51 +0200

    Remove unused References component

--- frontend/src/app/minimalist-parser/minimalist-parser.module.ts
+++ frontend/src/app/minimalist-parser/minimalist-parser.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { MinimalistParserComponent } from "./minimalist-parser.component";
 import { MinimalistParserAboutComponent } from "./minimalist-parser-about/minimalist-parser-about.component";
 import { MinimalistParserInputComponent } from "./minimalist-parser-input/minimalist-parser-input.component";
-import { MinimalistParserReferencesComponent } from "./minimalist-parser-references/minimalist-parser-references.component";
 import { MinimalistParserBrowserComponent } from "./minimalist-parser-browser/minimalist-parser-browser.component";
 import { SharedModule } from "../shared/shared.module";
 import { RouterModule } from "@angular/router";
@@ -14,7 +13,6 @@ import { IconButtonComponent } from "../shared/components/icon-button/icon-butto
         MinimalistParserComponent,
         MinimalistParserAboutComponent,
         MinimalistParserInputComponent,
-        MinimalistParserReferencesComponent,
         MinimalistParserBrowserComponent,
     ],
     imports: [SharedModule, RouterModule, NgOptimizedImage, IconButtonComponent],
